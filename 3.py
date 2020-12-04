97d1aac8abf18236b04fe739de8313465141179d
Trish Gillett <trish.gillett@shopify.com>
2020-12-04 09:57:05 +0900

    day 3

---
+++ 3.py
@@ -0,0 +1,36 @@
+with open('3-input.txt') as file:
+    data = file.read()
+
+lines = data.strip().split('\n')
+
+def count_trees(x_slope, y_slope):
+    tree_count = 0
+
+    x_pos, y_pos = 0, 0
+    while y_pos < len(lines):
+        line = lines[y_pos]
+        tree_count += int(line[x_pos % len(line)] == '#')
+        x_pos += x_slope
+        y_pos += y_slope
+
+    return tree_count
+
+print('part 1')
+print('answer: {}'.format(count_trees(3, 1)))
+
+print('\n')
+print('part 2')
+print('slope (1, 1): {}'.format(count_trees(1, 1)))
+print('slope (3, 1): {}'.format(count_trees(3, 1)))
+print('slope (5, 1): {}'.format(count_trees(5, 1)))
+print('slope (7, 1): {}'.format(count_trees(7, 1)))
+print('slope (1, 2): {}'.format(count_trees(1, 2)))
+
+print('answer: {}'.format(
+    count_trees(1, 1)
+    * count_trees(3, 1)
+    * count_trees(5, 1)
+    * count_trees(7, 1)
+    * count_trees(1, 2)
+))
+
